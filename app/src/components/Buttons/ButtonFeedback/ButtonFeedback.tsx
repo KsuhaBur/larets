e1a30395cbf4a53adb71144ae7941b78bb7acd36
KsuhaBur <79745273+KsuhaBur@users.noreply.github.com>
2024-07-04 21:26:29 +1000

    add link on WhatsApp

--- app/src/components/Buttons/ButtonFeedback/ButtonFeedback.tsx
+++ app/src/components/Buttons/ButtonFeedback/ButtonFeedback.tsx
@@ -5,7 +5,7 @@ import './ButtonFeedback.css';
 export default function ButtonFeedback() {
   return (
 
-      <Button name='описать ситуацию' link='/' />
+      <Button name='описать ситуацию' link='https://wa.me/79149757424?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5%21%20%D0%A5%D0%BE%D1%87%D1%83%20%D1%80%D0%B0%D1%81%D1%81%D0%BA%D0%B0%D0%B7%D0%B0%D1%82%D1%8C%20%D0%BE%20%D1%81%D0%B8%D1%82%D1%83%D0%B0%D1%86%D0%B8%D0%B8%2C%20%D0%BA%D0%BE%D1%82%D0%BE%D1%80%D0%B0%D1%8F%20%D0%BF%D1%80%D0%BE%D0%B8%D0%B7%D0%BE%D1%88%D0%BB%D0%B0' />
 
   )
 }
